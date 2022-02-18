5ed798bf44bdd2bc8232aa6827584a8e6aedd482
ruben1dlg <ruben1dlg@gmail.com>
2022-02-18 12:44:42 -0800

    Initial app

---
+++ dashboard_app.py
@@ -0,0 +1,33 @@
+from dash import Dash, html, dcc, Input, Output
+import altair as alt
+from vega_datasets import data
+
+
+# Read in global data
+df = data.gapminder()
+
+# Setup app and layout/frontend
+app = Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+server = app.server
+app.layout = html.Div([
+    html.Iframe(
+        id='scatter',
+        style={'border-width': '0', 'width': '100%', 'height': '400px'}),
+    dcc.Dropdown(
+        id='xcol-widget',
+        value='pop',  # REQUIRED to show the plot on the first page load
+        options=[{'label': col, 'value': col} for col in ['pop', 'life_expect', 'fertility']])])
+
+# Set up callbacks/backend
+@app.callback(
+    Output('scatter', 'srcDoc'),
+    Input('xcol-widget', 'value'))
+def plot_altair(xcol):
+    chart = alt.Chart(df).mark_point().encode(
+        x=xcol,
+        y='pop',
+        tooltip='country').interactive()
+    return chart.to_html()
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
